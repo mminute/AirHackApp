0f357fd34de9e199ef9f29ff5ddadc45af82c272
mminute <masondjennings@gmail.com>
2015-09-16 20:54:26 -0400

    Adds migration for ident, url, lat, and long.  Seeds from files for ident, url, lat/long

--- db/migrate/20150916182154_create_airports.rb
+++ db/migrate/20150916182154_create_airports.rb
@@ -3,6 +3,8 @@ class CreateAirports < ActiveRecord::Migration
     create_table :airports do |t|
       t.string :identifier
       t.string :airnav_url
+      t.float  :latitude
+      t.float :longitude
       t.timestamps null: false
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,44 @@ url_array = LinksFromHash.new(AllAirportUrls)
 url_array.grab_links
 links = url_array.all_links
 
+
+files_found = Dir["lib/airport_files/*"]
+
+
+files_opened = 0
+
 links.each do |link|
   if link[-4] == "/"
     ident = link[-3..-1]
   else
     ident = link[-4..-1]
   end
-  Airport.create(identifier: ident, airnav_url: link )
-end
 
+  file_path = "lib/airport_files/"
+  text_file = file_path + ident + ".txt"
+  file_contents_string = IO.readlines( text_file )[0]
+  info_hash = eval( file_contents_string )
+
+  lat_long = info_hash[:location]["Lat/Long:"][-2].split(" / ").map{|str| str.to_f }
+
+  # p file_contents_string
+  # files_opened = files_opened + 1
+
+  # p file_contents_string
+
+
+  # def missing_location_data
+  #   files_found = Dir[ file_path ]
+  #   [].tap do |no_location|
+  #     files_found.each do |path_and_file|
+  #       reader = IO.readlines( path_and_file )[0]
+  #       if !reader.include?(":location")
+  #         no_location << path_and_file
+  #       end
+  #     end
+  #   end
+  # end
+
+
+  Airport.create(identifier: ident, airnav_url: link, latitude: lat_long[0], longitude: lat_long[1] )
+end
