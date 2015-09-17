3380d8165d896a94781326123ced051388188ce4
mminute <masondjennings@gmail.com>
2015-09-16 21:40:51 -0400

    Adds default value of nil for most fields in schema

--- db/migrate/20150916182154_create_airports.rb
+++ db/migrate/20150916182154_create_airports.rb
@@ -6,23 +6,23 @@ class CreateAirports < ActiveRecord::Migration
       t.float  :latitude
       t.float  :longitude
       t.string :vfr_map, default: nil
-      t.string :airport_diagram_pdf_link
-      t.string :airport_diagram
-      t.string :notam_link
-      t.string :runway_info
-      t.string :airport_comms
-      t.string :airport_operations
-      t.string :vor
-      t.string :non_directional_beacon
-      t.string :airport_services
-      t.string :airport_ownership
-      t.string :airport_ops_stats
-      t.string :additional_remarks
-      t.string :nearby_airports_with_instrument_approaches
-      t.string :other_pages
-      t.string :where_to_stay
-      t.string :aviation_businesses
-      t.string :aerial_photo
+      t.string :airport_diagram_pdf_link, default: nil
+      t.string :airport_diagram, default: nil
+      t.string :notam_link, default: nil
+      t.string :runway_info, default: nil
+      t.string :airport_comms, default: nil
+      t.string :airport_operations, default: nil
+      t.string :vor, default: nil
+      t.string :non_directional_beacon, default: nil
+      t.string :airport_services, default: nil
+      t.string :airport_ownership, default: nil
+      t.string :airport_ops_stats, default: nil
+      t.string :additional_remarks, default: nil
+      t.string :nearby_airports_with_instrument_approaches, default: nil
+      t.string :other_pages, default: nil
+      t.string :where_to_stay, default: nil
+      t.string :aviation_businesses, default: nil
+      t.string :aerial_photo, default: nil
       t.timestamps null: false
     end
   end
